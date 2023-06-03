679f95732e56d7739a1f6590526d54ae8c1c4270
Derek Wan <d.wan@icloud.com>
2023-06-03 20:42:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `template-typescript`
+
+Template for TypeScript repositories
+
+## Getting started
+
+pnpm install
+pnpm run dev
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>TypeScript Template</h1>
+  </div>
+`;
