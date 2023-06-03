af7df2357e1c40dd181aa8dbd7132c4beaa46a7a
Derek Wan <d.wan@icloud.com>
2023-06-03 21:00:56 +0800

    Finish app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
+import solidJs from "@astrojs/solid-js";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({ integrations: [solidJs()] });
---
+++ src/service/PocketBase.ts
@@ -0,0 +1,3 @@
+import PocketBase from "pocketbase";
+
+export const POCKET = new PocketBase("http://127.0.0.1:8090");
